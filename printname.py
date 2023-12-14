b8c6a5e585d94dc4f75fce3ea6cb1718e4eb35ae
Sadiyamarfua9 <147970862+Sadiyamarfua9@users.noreply.github.com>
2023-12-14 23:16:02 +0530

    name

---
+++ printname.py
@@ -0,0 +1,7 @@
+name=input("Enter any name:\n")
+if name.isupper():
+    print(name,"is in uppercase letters")
+elif name.islower():
+    print(name,"is in lowercase letters")
+else:
+    print(name.upper())
