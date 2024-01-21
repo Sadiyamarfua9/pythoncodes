58e9ce65f5d292f77bc362e77430b2b6e5e44f4a
Sadiyamarfua9 <147970862+Sadiyamarfua9@users.noreply.github.com>
2024-01-21 15:36:02 +0530

    Update NameError.py

--- NameError.py
+++ NameError.py
@@ -8,3 +8,14 @@ try:
     print(fullnam)
 except NameError as err:
     print("error: ",err)
+
+#program2 for NameError
+try:
+  n1=int(input("enter number1:\n"))
+  n2=int(input("enter number2:\n"))
+  #here an attempt is being made to print the result after adding two numbers
+  print(res)
+ #since print statement is written before logic therefore an error raises
+res=n1+n2
+except NameError as ne:
+  print(f"{ne}")
